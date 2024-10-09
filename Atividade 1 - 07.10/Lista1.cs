d226a4739c05f9f4a9d12a8c91d48a32c3ba4876
LEONARDO PEREIRA LOPES <leonardo.plopes@senacsp.edu.br>
2024-10-08 21:57:26 -0300

    Avaliação

--- Atividade 1 - 07.10/Lista1.cs
+++ Atividade 1 - 07.10/Lista1.cs
@@ -8,5 +8,17 @@ namespace Atividade_1___07._10
 {
     internal class Lista1
     {
+        public static void escolherExercicio()
+        {
+            Console.WriteLine("Escolha a atividade que deseja executar: ");
+            Console.WriteLine();
+            int escolha = Convert.ToInt32(Console.ReadLine());
+            switch (escolha)
+            {
+                case 1:
+                    Atividade1()
+                        break;
+            }
+        }
     }
 }
